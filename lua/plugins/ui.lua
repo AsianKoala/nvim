3002df50bee041ed2879eb220fb77c1126971dc6
neil mehra <neilmehra@outlook.com>
2025-01-15 08:56:52 -0500

    change term dir

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -324,7 +324,7 @@ return {
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
-      direction = "horizontal",
+      direction = "tab",
       close_on_exit = true,
       shell = vim.o.shell,
       float_opts = {
