99b3fa771106e6964305aedbdd85dc27791abab0
neil mehra <neilmehra@outlook.com>
2025-02-10 01:35:13 -0500

    change render-markdown start event

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,7 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    ft = "md",
+    event = "VeryLazy",
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
